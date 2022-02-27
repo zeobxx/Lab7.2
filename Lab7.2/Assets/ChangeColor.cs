7990203d1c822f98d7c59a23916e71852ab17d97
zeobxx <meowmeow.tbz@gmail.com>
2022-02-27 15:36:07 +0700

    First commit

---
+++ Lab7.2/Assets/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void getRed(){
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void getBlue(){
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+    public void getGreen(){
+        this.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
---
+++ Lab7.2/Assets/ChangeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeModel : MonoBehaviour
+{
+   public void getAnimatorWalk(){
+       this.GetComponent<Animator>().Play("Catwalk Walk Turn 180 Tight");
+   }
+   public void getAnimatorDance(){
+       this.GetComponent<Animator>().Play("Dancing Maraschino Step");
+   }
+}
